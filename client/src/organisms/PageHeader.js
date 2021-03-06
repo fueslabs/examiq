038302b045aa5f4892ea5ce0d8dad6fa7d6fc5ab
Naval Monga <navalmonga04@gmail.com>
2021-03-06 10:04:35 -0600

    fix netlify form

--- client/src/organisms/PageHeader.js
+++ client/src/organisms/PageHeader.js
@@ -126,7 +126,7 @@ const PageHeader = (props) => {
         </Typography>
         <Typography variant="body1" className={classes.body}>AI that lets you convert notes to exams in minutes.</Typography>
         <Typography variant="overline" className={classes.monoText}>get early access</Typography>
-        <form method="post" name="waitlist" noValidate autoComplete="off" netlify onSubmit={(e) => submitWaitlist(e)}>
+        <form method="post" name="waitlist" noValidate autoComplete="off" data-netlify="true" onSubmit={(e) => submitWaitlist(e)}>
           <TextField 
             id="waitlist-form" 
             className={classes.waitlistRoot}
